85a5bddff611b8f1e5d7c09b1f734cdcca1ab181
Luciano Ricotta <luciano.ricotta@ngc.com>
2023-10-20 22:41:53 +0000

    update bash run forever

--- runBot.sh
+++ runBot.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 while :
 do
-    bot api_configs_crypto.yaml train_configs.yaml --crypto
+    bot api_configs_crypto.yaml train_configs_crypto.yaml --crypto
     bot api_configs.yaml train_configs.yaml
 done
