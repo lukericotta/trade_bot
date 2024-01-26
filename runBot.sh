983a53e4b9047ceb363af38f5a157515d429c5d1
Luciano Ricotta <luciano.ricotta@ngc.com>
2024-01-26 15:03:10 +0000

    log runBot.sh

--- runBot.sh
+++ runBot.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 while :
 do
-    git pull origin && pip install -e . && bot api_configs.yaml train_configs.yaml
-    sleep 3600
+	now=$EPOCHREALTIME
+	git pull origin && pip install -e . && bot api_configs.yaml train_configs.yaml > "output-$now.txt" 2>&1
+	git pull origin
+	git add "output-$now.txt"
+	git commit -m "output log for $now"
+	git push origin main
 done
