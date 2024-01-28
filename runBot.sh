e9c006c2b85ce85b6cfe77dedadd780de408abc3
Luciano Ricotta <luciano.ricotta@ngc.com>
2024-01-28 03:36:20 +0000

    update runBot script

--- runBot.sh
+++ runBot.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 while :
 do
-	now=$EPOCHREALTIME
+  now=$EPOCHREALTIME
   mkdir "logs/$now"
-	git pull origin
+  git pull origin
   pip install .
   bot api_configs.yaml train_configs.yaml --export > "logs/$now/output.txt" 2>&1
   cp sentiments.txt logs/$now/
   cp plot.png logs/$now/
-	git pull origin
-	git add "logs/*"
+  git pull origin
+  git add "logs/*"
   git add sentiments.txt
   git add plot.png
-	git commit -m "output logs for $now"
-	git push origin main
+  git commit -m "output logs for $now"
+  git push origin main
 done
