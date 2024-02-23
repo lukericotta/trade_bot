a56621cf7e66dee2a0b58839d4b76044c6fbffca
lukericotta <lucianoricotta1@gmail.com>
2024-02-22 18:21:32 -0600

    Update botFunctions.py

--- bot/botFunctions.py
+++ bot/botFunctions.py
@@ -40,7 +40,7 @@ def beforeHours(api):
     tz = pytz.timezone('US/Eastern')
     us_holidays = holidays.US()
     openTime = datetime.time(hour = 16, minute = 00, second = 0)
-    preOpenTime = datetime.time(hour = 15, minute = 55, second = 0)
+    preOpenTime = datetime.time(hour = 15, minute = 59, second = 0)
     now = datetime.datetime.now(tz)
     # If a holiday
     if now.strftime('%Y-%m-%d') in us_holidays:
@@ -50,7 +50,7 @@ def beforeHours(api):
     if now.date().weekday() > 4:
         print('Its a weekend')
         return False
-    # If before 1600 and after 1555
+    # If before 1600 and after 1559
     if (now.time() < openTime) and (now.time() > preOpenTime):
         print('Closing all position EOD')
         return close_all_positions_end_of_day(api)
@@ -63,11 +63,11 @@ def close_all_positions_end_of_day(api):
         #  Get the current time (New York time)
         time_now = datetime.datetime.now(pytz.timezone('US/Eastern'))
 
-        #  Calculate the timestamp for 15 mins before close (15:55) today.
+        #  Calculate the timestamp for 15 mins before close (15:59) today.
         five_mins_before_end_of_day = pd.Timestamp(year=time_now.year, month=time_now.month, day=time_now.day, hour=15,
-                                                   minute=55, second=00, tz='US/Eastern')
+                                                   minute=59, second=00, tz='US/Eastern')
 
-        #  If the current time is the same as (or after) 15:55, close all positions.
+        #  If the current time is the same as (or after) 15:59, close all positions.
         if time_now.isoformat() >= five_mins_before_end_of_day.isoformat():
             print('Closing all positions...')
             api.cancel_all_orders()
